b5045849c176722b0966b59abb2822384804de3c
Antoine <antoine@mbp-de-antoine.home>
2021-01-12 14:54:05 +0100

    inf.go new

--- inf.go
+++ inf.go
@@ -1,12 +1,27 @@
 package main
+
 import "fmt"
+
 type IPAddr [4]byte
+
+func (ip IPAddr) String() string {
+	var s string
+	for i:= range ip{
+		if(i==0){
+		s +=fmt.Sprint(int(ip[i]))
+		} else{
+			s +=". "+fmt.Sprint(int(ip[i]))
+		}
+	} 	
+
+	return s
+}
 func main() {
-hosts := map[string]IPAddr{
-"loopback": {127, 0, 0, 1},
-"googleDNS": {8, 8, 8, 8},
-}
-for name, ip := range hosts {
-fmt.Printf("%v: %v\n", name, ip)
-}
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
 }
